b67a6fdcd8b975657e9deb6d476fd96eb7ea7cda
Melvin Gamboa <116075886+melvingambie@users.noreply.github.com>
2022-12-09 04:11:37 +0800

    Application using Player Prefs

---
+++ PlayerPrefs/Assets/Scripts/SaveData.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class SaveData : MonoBehaviour
+{
+
+    public InputField textBox;
+
+    public void clickSave()
+
+    {
+        PlayerPrefs.SetString("username", textBox.text);
+       
+        SceneManager.LoadScene("Scene2");
+}
+
+}
